b2c0d1e41bf0f16ccbae243302e11159982c99c7
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-06-22 11:06:47 +0100

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+import HeroSection from "./components/landing/HeroSection";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,7 +1,41 @@
 import React from "react";
+import logo from "../../assets/images/rodLogoBlack.png";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-between items-center px-14 py-8">
+      <div
+        className={`flex flex-row items-center cursor-pointer ${styles.logo}`}
+      >
+        <a href="#Home">
+          <img src={logo} alt="logo" />
+        </a>
+        <p className={`ml-5 leading-5 font-black ${styles["logo-text"]}`}>
+          <a href="#Home">
+            Rodrigo <br />
+            De Lascio
+          </a>
+        </p>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <div className={`flex flex-row ${styles["navbar-links-container"]}`}>
+          <p>
+            <a href="#About">About</a>
+          </p>
+          <p>
+            <a href="#Skills">Skills</a>
+          </p>
+          <p>
+            <a href="#Project">Project</a>
+          </p>
+          <p>
+            <a href="#Contact">Contact</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
