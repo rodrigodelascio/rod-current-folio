fd8f77d1f674d0b7d17a8512aeefc5cb40e5d6bb
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-07-10 11:03:39 +0100

    responsive navbar implemented

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/images/rodLogoBlack.png";
 import { GoArrowUpRight } from "react-icons/go";
+import { CgMenu, CgClose } from "react-icons/cg";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const clickToHide = () => {
+    if (isMobile) {
+      setIsMobile(!isMobile);
+    }
+  };
+
   return (
     <div className={styles["main-navbar-container"]}>
       <div className={styles["logo-container"]}>
@@ -18,16 +27,40 @@ const Navbar = () => {
         </p>
       </div>
       <div className={styles["navbar-links-container"]}>
-        <div className={styles["inner-links-container"]}>
-          <a href="#About">About</a>
-          <a href="#Projects">Projects</a>
-          <a href="#Contact">Contact</a>
+        <div
+          className={
+            !isMobile
+              ? styles["inner-links-container"]
+              : `${styles["inner-links-container"]} ${styles["responsive-navbar"]}`
+          }
+        >
+          <a href="#About" onClick={clickToHide}>
+            About
+          </a>
+          <a href="#Projects" onClick={clickToHide}>
+            Projects
+          </a>
+          <a href="#Contact" onClick={clickToHide}>
+            Contact
+          </a>
+          <CgClose
+            size={30}
+            className={styles["hamburger-close-icon"]}
+            onClick={() => setIsMobile(!isMobile)}
+          />
         </div>
       </div>
       <div>
         <button className={styles["cv-button"]}>
           My CV <GoArrowUpRight className={styles.arrow} />
         </button>
+        <div className={styles["hamburger-menu-container"]}>
+          <CgMenu
+            size={30}
+            className={styles["hamburger-menu-icon"]}
+            onClick={() => setIsMobile(!isMobile)}
+          />
+        </div>
       </div>
     </div>
   );
