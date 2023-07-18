d1c5d2872641142cf14f59bdab1376a6e9c0e4f1
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-07-18 17:42:28 +0100

    about page text positioned

--- src/components/aboutpage/aboutHeroSection/AboutHeroSection.js
+++ src/components/aboutpage/aboutHeroSection/AboutHeroSection.js
@@ -11,7 +11,11 @@ const AboutHeroSection = () => {
         </div>
         <div className={styles["about-hero-text-wrapper"]}>
           <h1>About Me.</h1>
-          <h2>Hi there</h2>
+          <h2>
+            I'm a web developer based in
+            <br />
+            Walton-on-Thames, UK
+          </h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -19,17 +23,9 @@ const AboutHeroSection = () => {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum. Sed ut
-            perspiciatis unde omnis iste natus error sit voluptatem accusantium
-            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
-            inventore veritatis et quasi architecto beatae vitae dicta sunt
-            explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
-            aut odit aut fugit, sed quia consequuntur magni dolores eos qui
-            ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-            quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem.
+            culpa qui officia deserunt mollit anim id est laborum.
           </p>
+          <hr />
         </div>
       </div>
       <div className={styles["second-section-template"]}>
