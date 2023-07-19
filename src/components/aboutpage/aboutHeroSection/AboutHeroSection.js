0fcdde9eace680f252a4608878d279ce630abb04
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-07-19 14:02:15 +0100

    about me text improved

--- src/components/aboutpage/aboutHeroSection/AboutHeroSection.js
+++ src/components/aboutpage/aboutHeroSection/AboutHeroSection.js
@@ -18,13 +18,32 @@ const AboutHeroSection = () => {
           </h2>
           <p>
             Having come from a diverse background of studies and work
-            experiences, I have recently enrolled to study Computer Science at
-            Arden University. Passionate about technology, I love adding to my
-            tech stack, improving myself, and have been working on front-end
-            projects since 2022.
+            experiences, I decided to make a career change after over a decade
+            working in marketing and as a linguist. Passionate about technology,
+            I love adding to my tech stack, improving myself, and been working
+            on front-end projects since 2022.
           </p>
           <hr />
-          <div></div>
+          <div className={styles["past-present-future-container"]}>
+            <div className={styles["past-present-future-wrapper"]}>
+              <h3>The Past.</h3>
+              <p>
+                I have studied the foundations of Computer Science at a young
+                age and moved away from the area due to personal circumstances.
+                I have achieved a BA in Journalism and a Beng in Civil
+                Engineering afterwards.
+              </p>
+            </div>
+            <div className={styles["past-present-future-wrapper"]}>
+              <h3>The Present.</h3>
+              <p>
+                I found my way back to computing and as a lifelong learner,
+                after recently attending a Software Development Bootcamp, I
+                decided to go back to university and enhance my knowledge, to
+                become a better professional.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles["second-section-template"]}>
