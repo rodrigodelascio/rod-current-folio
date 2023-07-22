b84bf17b70cdb4ec10d587e76b0c3d2919f9692f
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-07-22 13:04:57 +0100

    projects page starter files

---
+++ src/components/projectspage/projectsHero/ProjectsHero.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./ProjectsHero.module.css";
+
+const ProjectsHero = () => {
+  return (
+    <div className={styles["projects-page-container"]}>
+      <div className={styles["projects-hero-container"]}>
+        <div className={styles["projects-hero-text-wrapper"]}>
+          <h1>Projects.</h1>
+          <h2>
+            Check out some of my <br />
+            projects and case studies
+          </h2>
+          <p>
+            I've worked on a range of personal projects and case studies,
+            including websites and apps.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsHero;
--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/common/Navbar";
+import ProjectsHero from "../components/projectspage/projectsHero/ProjectsHero";
 import Footer from "../components/common/Footer";
 
 const ProjectsPage = () => {
   return (
     <div>
       <Navbar />
+      <ProjectsHero />
       <Footer />
     </div>
   );
