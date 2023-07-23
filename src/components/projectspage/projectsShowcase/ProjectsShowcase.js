65b5336731a8cd30efd9c22331d98638002d9aa3
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-07-23 18:33:56 +0100

    projects showcase updated

--- src/components/projectspage/projectsShowcase/ProjectsShowcase.js
+++ src/components/projectspage/projectsShowcase/ProjectsShowcase.js
@@ -1,16 +1,22 @@
 import React from "react";
-import projectsShowcaseImg from "../../../assets/images/projectsShowcase.png";
+import sunnysideMac from "../../../assets/images/sunnysideMac.png";
 import styles from "./ProjectsShowcase.module.css";
 
 const ProjectsShowcase = () => {
   return (
     <div className={styles["projects-showcase-container"]}>
       <div className={styles["project-wrapper"]}>
-        <div className={styles["project-image-wrapper"]}>&nbsp;</div>
+        <div className={styles["project-image-wrapper"]}>
+          <img src={sunnysideMac} alt="imac screen with sunnyside website" />
+        </div>
         <div className={styles["project-description-wrapper"]}>
           <h3>Sunnyside Website</h3>
-          <p>A website</p>
-          <button>CASE STUDY</button>
+          <p>
+            The website for Sunnyside Creative Agency, built with HTML, CSS and
+            JavaScript, using Flexbox and optimized for the most common screen
+            sizes.
+          </p>
+          <button>Live Project</button>
         </div>
       </div>
     </div>
