2539cbe3465f434852bd702207aa3097076db100
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-07-25 17:59:10 +0100

    contact page starter files

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/common/Navbar";
+import ContactHero from "../components/contactpage/ContactHero";
 import Footer from "../components/common/Footer";
 
 const ContactPage = () => {
   return (
     <div>
       <Navbar />
+      <ContactHero />
       <Footer />
     </div>
   );
