83c0787b084cffd9b18d8e422bebb777ca659640
Rodrigo De Lascio <rodrigodelascio@gmail.com>
2024-05-16 13:04:09 +0100

    updates projects page with let it shine's website

--- src/components/projectspage/projectsShowcase/ProjectsShowcase.js
+++ src/components/projectspage/projectsShowcase/ProjectsShowcase.js
@@ -2,6 +2,7 @@ import React from "react";
 import sunnysideMac from "../../../assets/images/sunnysideMac.png";
 import rodweatherMac from "../../../assets/images/rodWeather.png";
 import rodKryptoMac from "../../../assets/images/rodKryptoMac.png";
+import letItShineMac from "../../../assets/images/letItShineMac.png";
 import { GoArrowUpRight } from "react-icons/go";
 import { Slide } from "react-awesome-reveal";
 import styles from "./ProjectsShowcase.module.css";
@@ -10,6 +11,42 @@ const ProjectsShowcase = () => {
   return (
     <div>
       <div className={styles["projects-showcase-container-odd"]}>
+        <div className={styles["project-wrapper"]}>
+          <Slide triggerOnce>
+            <div className={styles["project-image-wrapper"]}>
+              <img
+                src={letItShineMac}
+                alt="imac screen with sunnyside website"
+                className={styles["project-image"]}
+              />
+            </div>
+          </Slide>
+          <div className={styles["project-description-wrapper"]}>
+            <Slide direction="right" triggerOnce>
+              <h3>Let It Shine Website</h3>
+              <p>
+                From conception to the final product, the website for Let It
+                Shine Services, built according to customer's requirements, with
+                React, CSS Variables and optimized for the most common screen
+                sizes.
+              </p>
+              <a href="https://theletitshine.co.uk/" target="__blank">
+                <button>
+                  Live Project&nbsp;&nbsp;
+                  <GoArrowUpRight />
+                </button>
+              </a>
+              <h5>Tech Stack:</h5>
+              <div className={styles["tech-stack-wrapper"]}>
+                <h6>React</h6>
+                <h6>React Router</h6>
+                <h6>CSS</h6>
+              </div>
+            </Slide>
+          </div>
+        </div>
+      </div>
+      <div className={styles["projects-showcase-container-even"]}>
         <div className={styles["project-wrapper"]}>
           <Slide triggerOnce>
             <div className={styles["project-image-wrapper"]}>
@@ -42,14 +79,12 @@ const ProjectsShowcase = () => {
                 <h6>HTML</h6>
                 <h6>CSS</h6>
                 <h6>JavaScript</h6>
-                <h6>GIT</h6>
-                <h6>GitHub</h6>
               </div>
             </Slide>
           </div>
         </div>
       </div>
-      <div className={styles["projects-showcase-container-even"]}>
+      <div className={styles["projects-showcase-container-odd"]}>
         <div className={styles["project-wrapper"]}>
           <div className={styles["project-image-wrapper"]}>
             <Slide triggerOnce>
@@ -85,14 +120,12 @@ const ProjectsShowcase = () => {
                 <h6>OpenWeather API</h6>
                 <h6>Geo DB Cities API</h6>
                 <h6>Unsplash API</h6>
-                <h6>GIT</h6>
-                <h6>GitHub</h6>
               </div>
             </Slide>
           </div>
         </div>
       </div>
-      <div className={styles["projects-showcase-container-odd"]}>
+      <div className={styles["projects-showcase-container-even"]}>
         <div className={styles["project-wrapper"]}>
           <div className={styles["project-image-wrapper"]}>
             <Slide triggerOnce>
@@ -128,8 +161,6 @@ const ProjectsShowcase = () => {
                 <h6>CoinPaprika API</h6>
                 <h6>CoinGecko API</h6>
                 <h6>ExchangeRate API</h6>
-                <h6>GIT</h6>
-                <h6>GitHub</h6>
               </div>
             </Slide>
           </div>
